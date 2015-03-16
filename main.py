603bb23624d8718ad0df4a5ea3fd4279ccbb5ed1
Sunil Rottoo <sunil@datacratic.com>
2015-03-16 17:22:58 -0400

    create a simple function

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import os,socket,time,csv,json,datetime
+from datetime import datetime
+
+
+def generateStatsTables():
+	print "calling generate stats tables"
+	
 print ""
 print ""
 print "Running the tennis plugin!!!!"
 print "The time is now: " + str(datetime.now())
 print "hi this is nick I'm in your code"
+generateStatsTables()
